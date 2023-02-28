8d2ae9ee3719882bd89a796937e9ae22ec86189b
alshak-matar <115154259+alshak-matar@users.noreply.github.com>
2023-03-01 00:44:35 +0100

    Create job02.py

---
+++ jour01/job02.py
@@ -0,0 +1,6 @@
+class Operation:
+    def __init__(self,  num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+number = Operation(2, 3)
+print("num1 and ", number.num1,"\n","num2 and ",number.num2)
