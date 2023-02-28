07491ec6d74a3150de8f44cab170c2abe6f2e66a
alshak-matar <115154259+alshak-matar@users.noreply.github.com>
2023-03-01 00:50:38 +0100

    Create job03.py

---
+++ jour01/job03.py
@@ -0,0 +1,9 @@
+
+class Operation:
+    def __init__(self,num1,num2):
+        self.num = num1
+        self.num = num2
+    def addition(self):
+         print(self.num1 + self.num2)
+number = Operation(12, 3)
+number.addition()
