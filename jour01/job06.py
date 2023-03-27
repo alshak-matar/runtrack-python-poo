2a7dc6108497f6e48a4ed76bfff221796a400cda
alshak-matar <115154259+alshak-matar@users.noreply.github.com>
2023-03-27 19:16:42 +0200

    Update job06.py

--- jour01/job06.py
+++ jour01/job06.py
@@ -1,16 +1,27 @@
-class Animal():
+class Rectangle:
+    def __init__(self, longueur, largeur):
+        self.longueur = longueur
+        self.largeur = largeur
+    
+    def get_longueur(self):
+        return self.longueur
+    
+    def set_longueur(self, longueur):
+        self.longueur = longueur
+    
+    def get_largeur(self):
+        return self.largeur
+    
+    def set_largeur(self, largeur):
+        self.largeur = largeur
 
-    def __init__(self):
-        self.age = 0
-        self.prenom = ""
-    def vieillir(self):
-        self.age +=1
-    def nommer(self, prenom):
-        self.prenom = prenom
-animal1 = Animal()
-print(animal1.age,"ans")
-print(animal1.prenom)
-animal1.vieillir()
-animal1.nommer("Bob")
-print(animal1.age, "ans")
-print(animal1.prenom)
+mon_rectangle = Rectangle(10, 5)
+
+print("Longueur :", mon_rectangle.get_longueur())
+print("Largeur :", mon_rectangle.get_largeur())
+
+mon_rectangle.set_longueur(100)
+mon_rectangle.set_largeur(50)
+
+print("Longueur modifiée :", mon_rectangle.get_longueur())
+print("Largeur modifiée :", mon_rectangle.get_largeur())
