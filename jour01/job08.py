46a1ec5017f57f4948663baa41e1d18d291a112b
alshak-matar <115154259+alshak-matar@users.noreply.github.com>
2023-03-27 20:45:30 +0200

    Update job08.py

--- jour01/job08.py
+++ jour01/job08.py
@@ -1,24 +1,65 @@
-import math
+class Livre:
+    def __init__(self, titre, auteur, nb_pages):
+        self.titre = titre
+        self.auteur = auteur
+        self.nb_pages = nb_pages
+        self.disponible = True
+    
+    def get_titre(self):
+        return self.titre
+    
+    def set_titre(self, titre):
+        self.titre = titre
+    
+    def get_auteur(self):
+        return self.auteur
+    
+    def set_auteur(self, auteur):
+        self.auteur = auteur
+    
+    def get_nb_pages(self):
+        return self.nb_pages
+    
+    def set_nb_pages(self, nb_pages):
+        if isinstance(nb_pages, int) and nb_pages > 0:
+            self.nb_pages = nb_pages
+        else:
+            print("Erreur! le nombre de pages doit être un entier positif.")
+    
+    def get_disponible(self):
+        return self.disponible
+    
+    def verification(self):
+        return self.disponible
+    
+    def emprunter(self):
+        if self.disponible:
+            self.disponible = False
+            print("Le livre a été emprunté.")
+        else:
+            print("Le livre n'est pas disponible.")
 
 
+mon_livre = Livre("Get Riche", "Tolkien", 1137)
 
 
-class Cercle():
-    def __init__(self, rayon):
-        self.rayon = rayon
-    def changerRayon(self, rayon):
-        self.rayon = rayon
-    def circonference(self):
-        return((2 * math.pi) * self.rayon)
-    def aire(self):
-        return((self.rayon * self.rayon) * math.pi)
-    def diametre(self):
-        return(self.rayon * 2)
-    def afficherInfos(self):
-        print(self.aire())
-        print(self.diametre())
-        print(self.circonference())
-cerc1 = Cercle(4)
-cerc2 = Cercle(7)
-cerc1.afficherInfos()
-cerc2.afficherInfos()
+print("Titre :", mon_livre.get_titre())
+print("Auteur :", mon_livre.get_auteur())
+print("Nombre de pages :", mon_livre.get_nb_pages())
+
+
+mon_livre.set_titre("Le hobbit")
+mon_livre.set_nb_pages(300)
+
+
+print("Titre modifié :", mon_livre.get_titre())
+print("Nombre de pages modifié :", mon_livre.get_nb_pages())
+
+
+print("Le livre est disponible :", mon_livre.verification())
+
+
+mon_livre.emprunter()
+
+
+print("Le livre est disponible :", mon_livre.verification())
