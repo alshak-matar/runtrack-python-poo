a91a6728e606ebcecdb0455615e3aa9ac5dfe2ad
alshak-matar <115154259+alshak-matar@users.noreply.github.com>
2023-03-28 13:32:46 +0200

    Update job03.py

--- jour02/job03.py
+++ jour02/job03.py
@@ -1,55 +1,69 @@
-class Student:
-    def __init__(self, nom, prenom, id_etudiant, credit):
-        self.__nom = nom
-        self.__prenom = prenom
-        self.__id_etudaint = id_etudiant
-        self.__credit = credit
-        self.__level = self.__setstudentEval()
+class Rectangle:
+    def __init__(self, longueur, largeur):
+        self.longueur = longueur
+        self.largeur = largeur
+        
+    def perimetre(self):
+        return 2 * (self.longueur + self.largeur)
+        
+    def surface(self):
+        return self.longueur * self.largeur
+        
+    def get_longueur(self):
+        return self.longueur
+    
+    def set_longueur(self, nouvelle_longueur):
+        self.longueur = nouvelle_longueur
+        
+    def get_largeur(self):
+        return self.largeur
+    
+    def set_largeur(self, nouvelle_largeur):
+        self.largeur = nouvelle_largeur
 
-    def add_credis(self):
-        while True:
-            ajout_credit = input('Entrer le nombre de crédit a ajouter')
-            if ajout_credit < str(0) or ajout_credit.isalpha():
-                print('la valeur entrer doit être un nombre supérieur a 0')
-            else:
-                self.__credit += int(ajout_credit)
-                self.__level = self.__setstudentEval()
 
-    def __setstudentEval(self):
-        if self.__credit >= 90:
-            return 'Excellent'
-        if self.__credit >= 80:
-            return 'Très bien'
-        if self.__credit >= 70:
-            return 'Bien'
-        if self.__credit >= 60:
-            return 'Passable'
-        if self.__credit <60:
-            return 'Insuffisant'
-
-    def studentInfo(self):
-        print('nom =', self.__nom)
-        print('Prénom =', self.__prenom)
-        print('Id = ',self.__id_etudaint)
-        print('Niveau =',self.__level)
-
-    #method display class attributes
-    def afficherNom(self):
-        return self.__nom
-    def afficherPrenom(self):
-        return self.__prenom
-    def afficherID(self):
-        return self.__id_etudaint
-    def afficherCredit(self):
-        return self.__credit
+class Parallelepipede(Rectangle):
+    def __init__(self, longueur, largeur, hauteur):
+        super().__init__(longueur, largeur)
+        self.hauteur = hauteur
+        
+    def volume(self):
+        return self.surface() * self.hauteur
+        
+    def get_hauteur(self):
+        return self.hauteur
+    
+    def set_hauteur(self, nouvelle_hauteur):
+        self.hauteur = nouvelle_hauteur
 
 
 
+rectangle = Rectangle(5, 3)
 
-etudiant = Student('John', 'Doe', 145, 0)
-etudiant.add_credis()
-etudiant.add_credis()
-etudiant.add_credis()
-#the result
-print('le nombre de crédits de',etudiant.afficherNom(),etudiant.afficherPrenom(),'est de',etudiant.afficherCredit())
-etudiant.studentInfo()
+
+print("Périmètre :", rectangle.perimetre())
+print("Surface :", rectangle.surface())
+
+
+rectangle.set_longueur(8)
+rectangle.set_largeur(4)
+print("Nouvelle longueur :", rectangle.get_longueur())
+print("Nouvelle largeur :", rectangle.get_largeur())
+
+
+parallelepipede = Parallelepipede(5, 3, 2)
+
+
+print("Volume :", parallelepipede.volume())
+
+
+parallelepipede.set_longueur(8)
+parallelepipede.set_largeur(4)
+print("Nouvelle longueur :", parallelepipede.get_longueur())
+print("Nouvelle largeur :", parallelepipede.get_largeur())
+
+
+parallelepipede.set_hauteur(5)
+print("Nouvelle hauteur :", parallelepipede.get_hauteur())
+
+
